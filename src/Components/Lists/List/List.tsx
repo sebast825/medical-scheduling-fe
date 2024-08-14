e8f26d08729b2084d036a9363f4469b7ad341e2a
sebast825 <sebamolina825@gmail.com>
2024-08-14 14:52:20 -0300

    input list

--- src/Components/Lists/List/List.tsx
+++ src/Components/Lists/List/List.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import { ListGroup } from "react-bootstrap";
 import BackLink from "../../buttons/BackLink/BackLink";
-
+import "./List.scss"
 interface IList {
   listItems: {
     nombre: string;
@@ -31,14 +31,15 @@ function List({ listItems, handleSelect }: IList) {
   }, [buscarItem]);
 
   return (
-    < div className="m-2">
+    < div className="m-2  d-flex flex-column justify-content-center align-items-center">
       <input
-        className="form-control"
+        className="form-control  input-con-lupa"
         placeholder="Buscar"
         type="text"
         value={buscarItem}
         onChange={(e) => updateRegEx(e)}
-      />
+        style={{ width: "350px", minWidth: "300px"}}
+        />
       <br></br>
       <ListGroup className="gap-2 d-flex justify-content-center align-items-center">
         {mostrarItems.map((item) => (
@@ -46,7 +47,7 @@ function List({ listItems, handleSelect }: IList) {
           key= {item.id}
             action
             className="text-center"
-            style={{ maxWidth: "500px",minWidth:"300px" }}
+            style={{ maxWidth: "500px",width: "350px",minWidth:"300px" }}
             onClick={() => handleSelect(item.id)}
           >
             {item.nombre}
