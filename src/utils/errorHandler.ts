9fec018c124729978b4f40bc635e3a1129ec9604
sebast825 <sebamolina825@gmail.com>
2024-08-26 16:00:55 -0300

    handlError

--- src/utils/errorHandler.ts
+++ src/utils/errorHandler.ts
@@ -1,33 +1,13 @@
-import axios, { AxiosError } from 'axios';
-
-export function handleHttpError(error: object): string {
+export function handleHttpError(error: any): string {
   console.log(error)
+   let errorMessage = 'Un error inesperado ocurrió';
+   let errorCode = 500; // Default to 500 if status code is not available
 
-  if (axios.isAxiosError(error)) {
-    const axiosError = error as AxiosError;
-
-    if ( error.response != undefined ) {
-      const status = error.response.data.statusCode;
-      const msge =  error.response.data.message;
-      if (status === 400) {
-        return "400 - Error de validación: " + (msge || "Los datos proporcionados no son válidos.");
-      } else if (status === 401) {console.log("request")
-        return "401 - No tienes permiso para realizar esta acción.";
-      } else if (status === 403) {        
-        return "403 - Acceso prohibido: No tiene permisos para realizar esta acción.";
-      } else if (status === 404) {
-        return "" + (msge || "El recurso solicitado no existe.");
-      } else if (status >= 500) {
-        return "500 - Error del servidor: Por favor, intente más tarde.";
-      } else {
-        return "Error desconocido: " + (msge || "Ha ocurrido un error inesperado.");
-      }
-    } else if (error.request) {
-      return "Ha ocurrido un erorr";
-    } else {
-      return "Error de configuración: " + axiosError.message;
-    }
-  } else {
-    return "Error desconocido: Ha ocurrido un error inesperado.";
+  if(error.response?.data){
+    console.log("entra")
+    errorMessage = error.response.data;
+    errorCode = error.response.status;
   }
+
+return errorMessage;
 }
