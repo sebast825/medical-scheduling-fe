97280f64d2ffe9b56d8c7c7215d6289d3ec2395b
sebast825 <sebamolina825@gmail.com>
2024-12-15 13:24:29 -0300

    filtra Medicos

--- src/Components/Medico/ListaHorariosMedicos.tsx
+++ src/Components/Medico/ListaHorariosMedicos.tsx
@@ -10,6 +10,9 @@ function ListaHorariosMedicos() {
     Record<string, DisponibilidadMedico[]>
   >({});
   const [buscarItem, setbuscarItem] = useState<string>("");
+  const [horariosMedicosFiltrados, setHorariosMedicosFiltrados] = useState<
+    [string, DisponibilidadMedico[]][]
+  >([]);
 
   useEffect(() => {
     const executeAsyncTask = async () => {
@@ -18,11 +21,22 @@ function ListaHorariosMedicos() {
     executeAsyncTask();
   }, []);
 
+  //filtra los medicos
+  useEffect(() => {
+    const regEx = new RegExp(`^${buscarItem}`, "i");
+    const filteredItems = Object.entries(horariosMedicos).filter(
+      ([medico, horarios]) => regEx.test(medico)
+    );
+    setHorariosMedicosFiltrados(filteredItems);
+  }, [buscarItem, horariosMedicos]);
+
+
+
   async function getMedicos() {
     if (user != null) {
       var horariosAtencionMedicos: DisponibilidadMedico[] =
         await getDisponibilidadMedicos(user);
-        console.log(horariosAtencionMedicos)
+      console.log(horariosAtencionMedicos);
 
       var agruparHorariosPorMedico = await agruparObjetosPorClave(
         horariosAtencionMedicos,
@@ -42,53 +56,50 @@ function ListaHorariosMedicos() {
       });
     });
   }
+  
   function updateRegEx(e: any) {
     setbuscarItem(e.target.value);
   }
 
   return (
-    <div className="container d-flex  flex-column justify-content-center gap-3 " >
- 
-        <div className="row m-1" >
-          <input
-            className="form-control  input-con-lupa"
-            placeholder="Buscar por especialidad o médico"
-            type="text"
-            value={buscarItem}
-            onChange={(e) => updateRegEx(e)}
+    <div className="container d-flex  flex-column justify-content-center gap-3 ">
+      <div className="row m-1">
+        <input
+          className="form-control  input-con-lupa"
+          placeholder="Buscar por especialidad o médico"
+          type="text"
+          value={buscarItem}
+          onChange={(e) => updateRegEx(e)}
           //  style={{ width: "350px", minWidth: "300px" }}
-          />
-          </div>
-     
+        />
+      </div>
 
-          <div className=" d-flex flex-column flex-lg-row justify-content-center gap-3" >
-
-          
-          {Object.entries(horariosMedicos).map(([medico, horarios]) => (
-            <div className="col-12 col-lg-3  mb-3"  key={medico} >
-              <div className="card">
-                <div className="card-header text-center">
-                  <h5>{medico}</h5>
-                </div>
-                <div className="card-body ps-5 pe-5 ">
-                  {horarios.map((horario, index) => (
-                    <div className="row mb-2" key={index}>
-                      <div className=" col-6 ">
-                        <strong>{horario.diaSemana}</strong>
-                      </div>
-                      <div className=" col-6 text-center">
-                        <span>
-                          {horario.startTime} - {horario.endTime}
-                        </span>
-                      </div>
+      <div className=" d-flex flex-column flex-lg-row justify-content-center gap-3">
+        {horariosMedicosFiltrados.map(([medico, horarios]) => (
+          <div className="col-12 col-lg-3  mb-3" key={medico}>
+            <div className="card">
+              <div className="card-header text-center">
+                <h5>{medico}</h5>
+              </div>
+              <div className="card-body ps-5 pe-5 ">
+                {horarios.map((horario, index) => (
+                  <div className="row mb-2" key={index}>
+                    <div className=" col-6 ">
+                      <strong>{horario.diaSemana}</strong>
                     </div>
-                  ))}
-                </div>
+                    <div className=" col-6 text-center">
+                      <span>
+                        {horario.startTime} - {horario.endTime}
+                      </span>
+                    </div>
+                  </div>
+                ))}
               </div>
             </div>
-          ))}
-        </div>
-        </div>
+          </div>
+        ))}
+      </div>
+    </div>
   );
 }
 export default ListaHorariosMedicos;
