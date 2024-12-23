02cab6d83c5b51a1c36da20b797d1578f0ed484c
sebast825 <sebamolina825@gmail.com>
2024-12-23 17:34:21 -0300

    nuestros medicos format

--- src/pages/home/NuestrosMedicos.tsx
+++ src/pages/home/NuestrosMedicos.tsx
@@ -1,56 +1,67 @@
-import MedicosList from "../../Components/General/MedicosList/MedicosList";
-import { useEffect,useState } from "react";
+import { useEffect, useState } from "react";
 import { fetchMedicos } from "../../services/apiService";
 import { IMedicoResponse } from "../../types/Medico/MedicoResponse.type";
-import { Table } from 'react-bootstrap';
-import DataTable from "../../Components/General/MedicosList/MedicosList";
+import { Table } from "react-bootstrap";
+import Opening from "../../Components/General/Opening/Opening";
+import BackLink from "../../Components/buttons/BackLink/BackLink";
 
-function NuestrosMedicos(){
-   const [listaMedicos, setListaMedicos] = useState<IMedicoResponse[]>([]); 
-   const [error, setError] = useState(null);
 
-   useEffect(()=>{
-      const fetchData = async () => {
-         try {
-           const response : IMedicoResponse[] = await fetchMedicos();
-           setListaMedicos(response);
-         } catch (err : any)
-         {
-           setError(err.message || 'Error desconocido');
-         } finally {
-         }
-       };
-       
-       fetchData();
+function NuestrosMedicos() {
+  const [listaMedicos, setListaMedicos] = useState<IMedicoResponse[]>([]);
+  const [error, setError] = useState(null);
 
-},[])
-//filtra los medicos para tener la info encesaria
-var listaMedicosFiltered = listaMedicos.map(elem =>{
-   var obj = {nombre: elem.nombre + " " +elem.apellido,
-      especialidad: elem.especialidad
-   }
-   return obj;
-})
-//listaMedicosFiltered.forEach(elm => console.log(elm))
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response: IMedicoResponse[] = await fetchMedicos();
+        setListaMedicos(response);
+      } catch (err: any) {
+        setError(err.message || "Error desconocido");
+      } finally {
+      }
+    };
 
- return (
-   <Table striped bordered hover>
-   <thead>
-     <tr>
-       <th>Nombre</th>
-       <th>Especialidad</th>
-     </tr>
-   </thead>
-   <tbody>
-     {listaMedicos.map((item, index) => (
-       <tr key={index}>
-         <td>{item.apellido + " "+ item.nombre}</td>
-         <td>{item.especialidad}</td>
-       </tr>
-     ))}
-   </tbody>
- </Table>
- );
-};
+    fetchData();
+  }, []);
+  //filtra los medicos para tener la info encesaria
+  var listaMedicosFiltered = listaMedicos.map((elem) => {
+    var obj = {
+      nombre: elem.nombre + " " + elem.apellido,
+      especialidad: elem.especialidad,
+    };
+    return obj;
+  });
+  //listaMedicosFiltered.forEach(elm => console.log(elm))
+
+  return (
+    <>
+         <Opening title="Nuestros Medicos" smallOpening={false} />
+      <Table
+        striped
+        bordered
+        hover
+        className="container mt-5 overflow-hidden"
+        style={{ maxWidth: "700px"}}
+      >
+        <thead>
+          <tr className="text-center">
+            <th>Nombre</th>
+            <th>Especialidad</th>
+          </tr>
+        </thead>
+        <tbody >
+          {listaMedicos.map((item, index) => (
+            <tr className="text-center " key={index} >
+              <td      
+              >{item.apellido + " " + item.nombre }</td>
+              <td>{item.especialidad}</td>
+            </tr>
+          ))}
+        </tbody>
+      </Table>
+      <BackLink variant="primary" />
+    </>
+  );
+}
 
 export default NuestrosMedicos;
