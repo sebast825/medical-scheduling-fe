64e95725f25eff3fdc4a5903b17b1492f997b7d5
sebast825 <sebamolina825@gmail.com>
2024-12-25 20:01:26 -0300

    CrearUsuarioModal

---
+++ src/Components/modals/usuario/crearUsuarioModal.tsx
@@ -0,0 +1,78 @@
+import GenericModal from "../GenericModal/GenericModal";
+import { CreateUsuarioRequest } from "../../../types/usuario/CreateUsuarioRequest";
+import { Form } from "react-bootstrap";
+import { useState } from "react";
+
+
+interface ICrearUsuarioModal {
+  show: boolean;
+  handleClose: () => void;
+  handleConfirm: (personaResponse: CreateUsuarioRequest) => void;
+}
+
+function CrearUsuarioModal (props:ICrearUsuarioModal){
+   
+   const { show, handleClose, handleConfirm } = props;
+
+   const [nombre, setNombre] = useState<string>("");
+   const [password, setPassword] = useState<string>("");
+   const [email, setEmail] = useState<string>("");
+   const [confirmEmail, setConfirmEmail] = useState<string>("");
+
+   function confirmar (){
+
+   }
+
+   return (
+      <>
+      <GenericModal
+        show={show}
+        handleClose={handleClose}
+        handleConfirm={confirmar}
+        title="Editar Información Personal"
+      >
+        <Form className="d-flex flex-column" style={{ gap: "10px" }}>
+         
+          <Form.Group controlId="formBasicnumLicencia">
+            <Form.Label style={{ textAlign: "left" }}>Nombre Usuario</Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="Ingresar Nombre"
+              onChange={(e) => setNombre(e.target.value)}
+              value={nombre}
+            />
+          </Form.Group>
+          <Form.Group controlId="Contraseña">
+            <Form.Label style={{ textAlign: "left" }}>Contraseña</Form.Label>
+            <Form.Control
+              type="password"
+              placeholder="Ingresar contraseña"
+              onChange={(e) => setPassword(e.target.value)}
+              value={password}
+            />
+          </Form.Group>
+          <Form.Group controlId="Contraseña">
+            <Form.Label style={{ textAlign: "left" }}>Contraseña</Form.Label>
+            <Form.Control
+              type="email"
+              placeholder="Ingresar email"  
+                          onChange={(e) => setEmail(e.target.value)}
+              value={email}
+            />
+          </Form.Group>
+          <Form.Group controlId="ContraseñaEmail">
+            <Form.Label style={{ textAlign: "left" }}>Contraseña Email</Form.Label>
+            <Form.Control
+              type="email"
+              placeholder="Ingresar email"
+              onChange={(e) => setConfirmEmail(e.target.value)}
+              value={confirmEmail}
+            />
+          </Form.Group>
+        </Form>
+      </GenericModal>
+      </>
+    );
+}
+
+export default CrearUsuarioModal;
