104823363a6e64cb19c1ecd0db810ba34cbac4cb
sebast825 <sebamolina825@gmail.com>
2024-12-26 15:27:16 -0300

    putPersona Logic en Hook

--- src/hooks/personas/usePersonas.tsx
+++ src/hooks/personas/usePersonas.tsx
@@ -1,32 +1,46 @@
 import { useCallback, useEffect, useState } from "react";
 import { IPersonaUpdate } from "../../types/Persona/PersonaUpdate.type";
 import { IPersonaResponse } from "../../types/Persona/PersonaResponse.type";
-import { useUserInfo } from "../../context/authContext";
+import { usePacienteContext, usePersonaInfoContext, useUserInfo } from "../../context/authContext";
 import { IMedicoResponse } from "../../types/Medico/MedicoResponse.type";
 import { ErrorTypeAny } from "../../types/Error.type";
-import { fecthUpdateEstadoUsuarioYPersona, fetchUpdatePersona } from "../../services/apiService";
+import {
+  fecthUpdateEstadoUsuarioYPersona,
+  fetchUpdatePersona,
+} from "../../services/apiService";
 import useToastit from "../useToastit";
 import GetJwtContent from "../../utils/jwtUtils";
+import IPacienteResponse from "../../types/Paciente/PacienteResponse.type";
 
 function usePersonas() {
   const user = useUserInfo();
   const [persona, setPersona] = useState<IPersonaResponse>();
   const [errorPersona, setError] = useState<ErrorTypeAny>(null);
+  const { personaInfo } = usePersonaInfoContext();
+  const { pacienteInfo, setPacienteInfo } = usePacienteContext();
+  const { setPersonaInfo } = usePersonaInfoContext();
 
-   
-  const putPersona = useCallback(async (dto: IPersonaUpdate,userId : string) => {
+  const putPersona = useCallback(async (dto: IPersonaUpdate) => {
+
+    if (personaInfo == null) return undefined;
     let validarDatos = validarPersona(dto);
-    if(validarDatos!= null){
-      error(validarDatos)
+    if (validarDatos != null) {
+      error(validarDatos);
       return;
     }
 
     try {
       if (user == null) return;
-      const response: IPersonaResponse = await fetchUpdatePersona(user, dto,userId);
-      //setPersona(response);
-      return response;
+      const response: IPersonaResponse = await fetchUpdatePersona(
+        user,
+        dto,
+        personaInfo.id.toString()
+      );
+      if (response != null) {
+        actualizarPacienteFe(response)
 
+      }
+      return response;
     } catch (err: any) {
       if (err.response && err.response.status === 401) {
         setError(err.response.data || "Error desconocido");
@@ -35,50 +49,63 @@ function usePersonas() {
       }
     }
   }, []);
+  function  actualizarPacienteFe(updatedPersona: IPersonaResponse) {
+    setPacienteInfo((prevInfo: IPacienteResponse | null) => ({
+      ...updatedPersona,
+      telefonoEmergencia: prevInfo?.telefonoEmergencia ?? "",
+      nombreEmergencia: prevInfo?.nombreEmergencia ?? "",
+    }));
+    setPersonaInfo(updatedPersona);
+  }
+  function validarPersona(dto: IPersonaUpdate) {
+    if (!dto.nombre || dto.nombre.length < 2 || dto.nombre.length > 100)
+      return "El nombre debe tener entre 2 caracteres y 100 caracteres.";
+    if (!dto.apellido || dto.apellido.length < 2 || dto.nombre.length > 100)
+      return "El apellido debe tener entre 2 caracteres y 100 caracteres.";
+    if (!/^\d{8,15}$/.test(dto.telefono))
+      return "El teléfono debe contener entre 8 y 15 dígitos.";
+    if (!dto.sexoId || dto.sexoId < 1 || dto.sexoId > 3)
+      return "El sexo no es válido.";
+    if (!dto.numeroDocumento || dto.numeroDocumento.length != 8)
+      return "El numero de documento es invalido";
 
-  function validarPersona (dto : IPersonaUpdate){
-
-    if (!dto.nombre || dto.nombre.length < 2 || dto.nombre.length > 100) return "El nombre debe tener entre 2 caracteres y 100 caracteres.";
-    if (!dto.apellido || dto.apellido.length < 2 || dto.nombre.length > 100) return "El apellido debe tener entre 2 caracteres y 100 caracteres.";
-    if (!/^\d{8,15}$/.test(dto.telefono)) return "El teléfono debe contener entre 8 y 15 dígitos.";
-    if (!dto.sexoId || dto.sexoId < 1 || dto.sexoId > 3) return "El sexo no es válido.";
-    if(!dto.numeroDocumento || dto.numeroDocumento.length != 8) return "El numero de documento es invalido"
-
-    const fechaNac = new Date(dto.fechaNacimiento); 
-    const fechaActual = new Date(); 
+    const fechaNac = new Date(dto.fechaNacimiento);
+    const fechaActual = new Date();
     if (fechaNac >= fechaActual) {
       return "La fecha de nacimiento debe ser anterior a la fecha actual.";
     }
   }
 
-  const updateEstadoPersonaYUsuario = useCallback(async (id : number, estadoId: number) => {
-  
-    try {
-      if (user == null) return;
-      const response: IPersonaResponse = await fecthUpdateEstadoUsuarioYPersona(user, id,estadoId);
-      //setPersona(response);
-      return response;
-
-    } catch (err: any) {
-      if (err.response && err.response.status === 401) {
-        setError(err.response.data || "Error desconocido");
-      } else {
-        setError(err.response.data);
+  const updateEstadoPersonaYUsuario = useCallback(
+    async (id: number, estadoId: number) => {
+      try {
+        if (user == null) return;
+        const response: IPersonaResponse =
+          await fecthUpdateEstadoUsuarioYPersona(user, id, estadoId);
+        //setPersona(response);
+        return response;
+      } catch (err: any) {
+        if (err.response && err.response.status === 401) {
+          setError(err.response.data || "Error desconocido");
+        } else {
+          setError(err.response.data);
+        }
       }
-    }
-  }, []);
+    },
+    []
+  );
 
-  const {error} = useToastit();
+  const { error } = useToastit();
 
-  useEffect(()=>{
-   if(errorPersona == null) return
-     error(errorPersona);
-  },[errorPersona])
-  
-  return{
-   putPersona,
-   updateEstadoPersonaYUsuario
-  }
+  useEffect(() => {
+    if (errorPersona == null) return;
+    error(errorPersona);
+  }, [errorPersona]);
+
+  return {
+    putPersona,
+    updateEstadoPersonaYUsuario,
+  };
 }
 
 export default usePersonas;
--- src/pages/paciente/InformacionPersonal.tsx
+++ src/pages/paciente/InformacionPersonal.tsx
@@ -1,28 +1,17 @@
 import Opening from "../../Components/General/Opening/Opening";
 import PersonaInfoCard from "../../Components/General/Cards/PersonaInfoCard/PersonaInfoCard";
-import {
-  usePacienteContext,
-  usePersonaInfoContext,
-  useUserInfo,
-} from "../../context/authContext";
+import { useUserInfo } from "../../context/authContext";
 import { useEffect } from "react";
 import useRedirects from "../../hooks/useRedicrects";
 import PacienteInfoCard from "../../Components/General/Cards/PacienteInfoCard/PacienteInfoCard";
-import useWindowSize from "../../hooks/ScreenSize";
 import "../estiloCompartido.scss";
-import IPacienteResponse from "../../types/Paciente/PacienteResponse.type";
 import { IPersonaResponse } from "../../types/Persona/PersonaResponse.type";
 import { IPersonaUpdate } from "../../types/Persona/PersonaUpdate.type";
 import usePersonas from "../../hooks/personas/usePersonas";
 
 function InformaciónPersonal() {
   const user = useUserInfo();
-  // const redirectToLogin = useRedirectToLogin();
-
   const { redirectToLogin } = useRedirects();
-  const { pacienteInfo, setPacienteInfo } = usePacienteContext();
-  const { setPersonaInfo } = usePersonaInfoContext();
-  const { personaInfo } = usePersonaInfoContext();
   const { putPersona } = usePersonas();
 
   useEffect(() => {
@@ -31,36 +20,10 @@ function InformaciónPersonal() {
     }
   }, []);
 
-  async function updatePersona(
-    persona: IPersonaUpdate
-  ): Promise<IPersonaResponse | undefined> {
-    if (personaInfo == null) return undefined;
+  async function handlePersonaUpdate(persona: IPersonaUpdate) {
     var updatedPersona: IPersonaResponse | undefined = await putPersona(
-      persona,
-      personaInfo.id.toString()
+      persona
     );
-    return updatedPersona;
-  }
-  async function laLlama(persona: IPersonaUpdate) {
-    var updatedPersona = await updatePersona(persona);
-
-    if (updatedPersona == null) {
-      //error("Ha ocurrido un error, no se pudo completar la accion con éxito.");
-      return;
-    }
-    handlePersonaUpdate(updatedPersona);
-    //handleConfirm(updatedPersona);
-  }
-
-  function handlePersonaUpdate(updatedPersona: IPersonaResponse) {
-    if (pacienteInfo == null) return;
-
-    setPacienteInfo((prevInfo: IPacienteResponse | null) => ({
-      ...updatedPersona,
-      telefonoEmergencia: prevInfo?.telefonoEmergencia ?? "",
-      nombreEmergencia: prevInfo?.nombreEmergencia ?? "",
-    }));
-    setPersonaInfo(updatedPersona);
   }
 
   return (
@@ -70,7 +33,7 @@ function InformaciónPersonal() {
         className="d-flex  informacionPersonal justify-content-center flex-md-row flex-column gap-5 mt-5 mb-5"
         style={{ width: "min-content", margin: "auto" }}
       >
-        <PersonaInfoCard handleConfirm={laLlama} />
+        <PersonaInfoCard handleConfirm={handlePersonaUpdate} />
 
         <PacienteInfoCard handleEvent={true} />
       </div>
