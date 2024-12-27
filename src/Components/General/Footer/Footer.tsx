c40b1e34d09510cf7d1a8664eee3034c133aebbf
sebast825 <sebamolina825@gmail.com>
2024-12-27 19:00:05 -0300

    footer

--- src/Components/General/Footer/Footer.tsx
+++ src/Components/General/Footer/Footer.tsx
@@ -1,36 +1,39 @@
+import LineTitle from "../LineTitle/LineTitle";
 import "./Footer.scss";
 
 function Footer() {
-
   return (
-    <div className="footer text-center">
-        <p>
-          Este sitio web es una demostración de una aplicación de gestión de
-          turnos médicos. El acceso a este sitio es exclusivo para fines de
-          demostración y evaluación. Los datos presentados en este sitio son
-          ficticios y se utilizan únicamente con fines demostrativos.
-        </p>
-        <p>
-          Importante: Este sitio no tiene ninguna relación con sistemas reales
-          de gestión de turnos médicos ni con ninguna entidad médica o
-          profesional. Los datos de pacientes, médicos y turnos son ejemplos
-          generados para ilustrar el funcionamiento de la aplicación.
-        </p>
-      <div className="pt-4 ">
-        <h6>
-          Desarrollado por Sebastián Molina
-        
-        </h6>
+    <div className="footer text-center d-flex gap-3  flex-column">
+      <div>
+        <img src="/images/logoWhite.png" style={{ height: "60px" }}></img>
+      </div>
+      
+      <div className=" d-flex align-self-center flex-column " style={{  maxWidth:"1100px"}}>
+      <p>
+        Este sitio web es una demostración de una aplicación de gestión de
+        turnos médicos. El acceso a este sitio es exclusivo para fines de
+        demostración y evaluación. Los datos presentados en este sitio son
+        ficticios y se utilizan únicamente con fines demostrativos.
+      </p>
+      <p>
+        Importante: Este sitio no tiene ninguna relación con sistemas reales de
+        gestión de turnos médicos ni con ninguna entidad médica o profesional.
+        Los datos de pacientes, médicos y turnos son ejemplos generados para
+        ilustrar el funcionamiento de la aplicación.
+      </p>
+      </div>
+      <LineTitle/>
 
-        <h6>
-        <a
+      <div >
+      <h5>Desarrollado por <span style={{fontStyle: "italic"}}>Sebastián Molina</span></h5>
+      <h6>
+          <a
             href="https://www.linkedin.com/in/sebastian-molina97/"
             className="link"
             target="_blank"
           >
             Contacto
           </a>
-
         </h6>
       </div>
     </div>
