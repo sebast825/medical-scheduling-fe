b7b34d4371c511effa8fef10204a80b2da5ccb3d
sebast825 <sebamolina825@gmail.com>
2024-12-27 20:48:17 -0300

    scroll nav bar

--- src/Components/General/Nav Bar/NavBar.tsx
+++ src/Components/General/Nav Bar/NavBar.tsx
@@ -1,71 +1,102 @@
-import { useEffect, useState } from 'react';
-import Nav from 'react-bootstrap/Nav';
-import { useUserInfo } from '../../../context/authContext';
-import useRedicrects from '../../../hooks/useRedicrects';
-import SideMenu from '../sideMenu/SideMenu';
-import useRediectHomeByRole from '../../../hooks/roles/useRediectHomeByRole';
+import { useEffect, useState } from "react";
+import Nav from "react-bootstrap/Nav";
+import { useUserInfo } from "../../../context/authContext";
+import useRedicrects from "../../../hooks/useRedicrects";
+import SideMenu from "../sideMenu/SideMenu";
+import useRediectHomeByRole from "../../../hooks/roles/useRediectHomeByRole";
+import "./NavBar.scss"
 
 function NavBar() {
-   const [activeKey, setActiveKey] = useState<string>('link');
-   const [isAuthenticated, setIsAuthenticated] = useState<boolean>(false);
-   const user = useUserInfo();
-   const rhandleRedirectHome = useRediectHomeByRole()
+  const [activeKey, setActiveKey] = useState<string>("link");
+  const [isAuthenticated, setIsAuthenticated] = useState<boolean>(false);
+  const user = useUserInfo();
+  const rhandleRedirectHome = useRediectHomeByRole();
 
-  const {redirectToNuestrosMedicos,redirectToLogin,redirectToHome} = useRedicrects();
+  const { redirectToNuestrosMedicos, redirectToLogin, redirectToHome } =
+    useRedicrects();
 
+  useEffect(() => {
+    if (user != null) {
+      setIsAuthenticated(true);
+    }
+  }, [user]);
 
-   useEffect(()=>{
-      if(user != null){
-        setIsAuthenticated(true)
+ 
+
+  const handleSelect = (selectedKey: string | null) => {
+    if (selectedKey) {
+      setActiveKey(selectedKey);
+    }
+  };
+
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+  const [isVisible,setIsVisible] = useState<boolean>(true);
+  const [lastScrollY, setLastScrollY] = useState<number>(0);
+  useEffect(() => {
+
+    const handleScroll = () => {
+
+      const handleScroll = () =>{
+  
       }
-   },[user])
+      let escrollDown : boolean = lastScrollY < window.scrollY;
 
-   useEffect(()=>{console.log(isAuthenticated)},[isAuthenticated])
-
-   const handleSelect = (selectedKey: string | null) => {
-      if (selectedKey) {
-        setActiveKey(selectedKey);
+      if(escrollDown){
+        setIsVisible(false)
+      }else{
+        setIsVisible(true)
       }
+      console.log(escrollDown,lastScrollY)
+      setLastScrollY(window.scrollY)
+
+    }
+    window.addEventListener("scroll", handleScroll)
+    
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
     };
-   
-    const [show, setShow] = useState(false);
+  }, [lastScrollY]);
 
-    const handleClose = () => setShow(false);
-    const handleShow = () => setShow(true);
+
   return (
-    
-    
-
-    <Nav fill variant="tabs" activeKey={activeKey} onSelect={handleSelect} className='d-flex  align-items-center'>
+    <Nav
+      fill
+      variant="tabs"
+      activeKey={activeKey}
+      onSelect={handleSelect}
+      className={`d-flex  align-items-center navBar ${isVisible ? "showNavBar" : "hideNavBar"}`}
+    >
       <Nav.Item>
-        <Nav.Link onClick={rhandleRedirectHome} >
-          <img src="/images/logo.png" alt="Logo"  style={{ height: '40px'}} />
+        <Nav.Link onClick={rhandleRedirectHome}>
+          <img src="/images/logo.png" alt="Logo" style={{ height: "40px" }} />
         </Nav.Link>
       </Nav.Item>
-      
+
       <Nav.Item>
-        <Nav.Link onClick={redirectToNuestrosMedicos} eventKey="a" >Nuestros Medicos</Nav.Link>
+        <Nav.Link onClick={redirectToNuestrosMedicos} eventKey="a">
+          Nuestros Medicos
+        </Nav.Link>
       </Nav.Item>
-      { !isAuthenticated ? (
+      {!isAuthenticated ? (
         <>
-    
-
-
-      <Nav.Item>
-        <Nav.Link onClick={redirectToLogin}  eventKey="b" >Ingresar</Nav.Link>
-      </Nav.Item>   
+          <Nav.Item>
+            <Nav.Link onClick={redirectToLogin} eventKey="b">
+              Ingresar
+            </Nav.Link>
+          </Nav.Item>
         </>
-
-       
-       ) :(
-           <Nav.Item>
-        <Nav.Link eventKey="c" onClick={handleShow}>Mi Perfil</Nav.Link>
-      </Nav.Item>    
+      ) : (
+        <Nav.Item>
+          <Nav.Link eventKey="c" onClick={handleShow}>
+            Mi Perfil
+          </Nav.Link>
+        </Nav.Item>
       )}
-          <SideMenu show={show} handleClose={handleClose}/>
-
+      <SideMenu show={show} handleClose={handleClose} />
     </Nav>
-    
   );
 }
 
