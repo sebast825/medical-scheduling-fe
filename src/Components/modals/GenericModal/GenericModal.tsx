76842a3dd7050848d01fc2e4d255a25a35a82eb7
sebast825 <sebamolina825@gmail.com>
2024-12-27 23:42:23 -0300

    more colors

--- src/Components/modals/GenericModal/GenericModal.tsx
+++ src/Components/modals/GenericModal/GenericModal.tsx
@@ -47,7 +47,7 @@ function GenericModal({
         ))} */}
       </Modal.Body>
       <Modal.Footer>
-        <Button variant="secondary" onClick={handleClose}>
+        <Button variant="dark" onClick={handleClose}>
           Cancelar
         </Button>
         <Button variant="primary" onClick={handleConfirm}>
