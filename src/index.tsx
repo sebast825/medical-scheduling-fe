2aa8312f0827964af292d18d5ccbe49b419e1850
sebast825 <sebamolina825@gmail.com>
2024-12-27 23:07:19 -0300

    added custom boostrap

--- src/index.tsx
+++ src/index.tsx
@@ -3,8 +3,8 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import 'bootstrap/dist/css/bootstrap.min.css'; // Importa los estilos de Bootstrap
-
+//import 'bootstrap/dist/css/bootstrap.min.css'; // Importa los estilos de Bootstrap
+import './styles/bootstrap.scss'
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
