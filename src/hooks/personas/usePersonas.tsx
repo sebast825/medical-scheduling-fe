9112de43cd6dd4493d7254568e64d1abce521938
sebast825 <sebamolina825@gmail.com>
2024-12-30 14:22:08 -0300

    toastit

--- src/hooks/personas/usePersonas.tsx
+++ src/hooks/personas/usePersonas.tsx
@@ -1,7 +1,11 @@
 import { useCallback, useEffect, useState } from "react";
 import { IPersonaUpdate } from "../../types/Persona/PersonaUpdate.type";
 import { IPersonaResponse } from "../../types/Persona/PersonaResponse.type";
-import { usePacienteContext, usePersonaInfoContext, useUserInfo } from "../../context/authContext";
+import {
+  usePacienteContext,
+  usePersonaInfoContext,
+  useUserInfo,
+} from "../../context/authContext";
 import { IMedicoResponse } from "../../types/Medico/MedicoResponse.type";
 import { ErrorTypeAny } from "../../types/Error.type";
 import {
@@ -11,19 +15,17 @@ import {
 import useToastit from "../useToastit";
 import GetJwtContent from "../../utils/jwtUtils";
 import IPacienteResponse from "../../types/Paciente/PacienteResponse.type";
+import { handleHttpError } from "../../utils/errorHandler";
 
 function usePersonas() {
   const user = useUserInfo();
-  const [persona, setPersona] = useState<IPersonaResponse>();
   const [errorPersona, setError] = useState<ErrorTypeAny>(null);
   const { personaInfo } = usePersonaInfoContext();
-  const { pacienteInfo, setPacienteInfo } = usePacienteContext();
   const { setPersonaInfo } = usePersonaInfoContext();
+  const { error, success } = useToastit();
 
   const putPersona = useCallback(async (dto: IPersonaUpdate) => {
-
     if (personaInfo == null) return undefined;
- 
 
     try {
       if (user == null) return;
@@ -32,14 +34,10 @@ function usePersonas() {
         dto,
         personaInfo.id.toString()
       );
-     
+      success("Informacion actualizada exitosamente.");
       return response;
     } catch (err: any) {
-      if (err.response && err.response.status === 401) {
-        setError(err.response.data || "Error desconocido");
-      } else {
-        setError(err.response.data);
-      }
+      error(handleHttpError(err));
     }
   }, []);
   async function handlePersonaUpdate(persona: IPersonaUpdate) {
@@ -47,20 +45,13 @@ function usePersonas() {
       persona
     );
     if (updatedPersona != null) {
-      actualizarPacienteFe(updatedPersona)
-
+      actualizarPacienteFe(updatedPersona);
     }
   }
-  
-  function  actualizarPacienteFe(updatedPersona: IPersonaResponse) {
-    setPacienteInfo((prevInfo: IPacienteResponse | null) => ({
-      ...updatedPersona,
-      telefonoEmergencia: prevInfo?.telefonoEmergencia ?? "",
-      nombreEmergencia: prevInfo?.nombreEmergencia ?? "",
-    }));
+
+  function actualizarPacienteFe(updatedPersona: IPersonaResponse) {
     setPersonaInfo(updatedPersona);
   }
-
 
   const updateEstadoPersonaYUsuario = useCallback(
     async (id: number, estadoId: number) => {
@@ -68,25 +59,16 @@ function usePersonas() {
         if (user == null) return;
         const response: IPersonaResponse =
           await fecthUpdateEstadoUsuarioYPersona(user, id, estadoId);
-        //setPersona(response);
+        success("Estado actualizado exitosamente");
+
         return response;
       } catch (err: any) {
-        if (err.response && err.response.status === 401) {
-          setError(err.response.data || "Error desconocido");
-        } else {
-          setError(err.response.data);
-        }
+
+        error(handleHttpError(err));
       }
     },
     []
   );
-
-  const { error } = useToastit();
-
-  useEffect(() => {
-    if (errorPersona == null) return;
-    error(errorPersona);
-  }, [errorPersona]);
 
   return {
     handlePersonaUpdate,
