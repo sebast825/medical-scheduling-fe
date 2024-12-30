6b2ccc6abc5d14f23091440aa7210ed0cd81d916
sebast825 <sebamolina825@gmail.com>
2024-12-29 23:33:13 -0300

    TitleContent

--- src/Components/Medico/TurnosList/TurnosListMedico.tsx
+++ src/Components/Medico/TurnosList/TurnosListMedico.tsx
@@ -25,22 +25,27 @@ function TurnosListMedico() {
 
   return (
     <>
-      <div style={{ maxWidth: "1200px", margin: "auto" }} className="pt-3 pb-3 pt-md-4 pb-md-5">
-        <TitleContent title="Turnos para hoy" pading={false} />
+      <div
+        style={{ maxWidth: "1200px", margin: "auto" }}
+        className="pt-3 pb-3 pt-md-4 pb-md-5"
+      >
         {turnos && turnos.length > 0 ? (
-          turnos.map((turno: TurnoResponse) => (
-            <CardTurnoMedico
-              key={turno.id}
-              turno={turno}
-              btnEvent={(e) => updateStatusTurno(e)}
-            />
-          ))
+          <>
+            <TitleContent title="Turnos para hoy" pading={false} />
+
+            {turnos.map((turno: TurnoResponse) => (
+              <CardTurnoMedico
+                key={turno.id}
+                turno={turno}
+                btnEvent={(e) => updateStatusTurno(e)}
+              />
+            ))}
+          </>
         ) : (
-          <div className="container d-flex justify-content-center p-md-5 pt-5 p-2">
-            <h2 className="text-center mb-4 border-bottom pb-2">
-              Hoy no tenés turnos agendados
-            </h2>
-          </div>
+
+          <TitleContent title=" Hoy no tenés turnos agendados" />
+
+          
         )}
       </div>
     </>
