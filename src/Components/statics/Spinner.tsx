00e77f3f56f3aa5e984e19c329f086721ce99ad6
sebast825 <sebamolina825@gmail.com>
2024-12-30 20:33:36 -0300

    spinner

--- src/Components/statics/Spinner.tsx
+++ src/Components/statics/Spinner.tsx
@@ -1,5 +1,16 @@
-export function Spinner (){
-   return (
-      <div>Cargando</div>
-   )
+import "./Spinner.scss";
+
+export function Spinner() {
+  return (
+    <div className="contenedor d-flex justify-content-center align-items-center flex-column">
+      <div className=" spinner-wrapper">
+
+      <img src="/images/logoWhite.png" className="spinner-bounce  " alt="Logo" style={{ height: "100px" }} />
+
+      </div>
+
+      <h2 className="text title">Clinica Horizonte</h2>
+      <h4 className="text subTitle">Cargando</h4>
+    </div>
+  );
 }
