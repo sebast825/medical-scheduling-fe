83ee5ab54d303e6ba0f94e923f6f3fb4d3d5eca4
sebast825 <sebamolina825@gmail.com>
2025-01-02 19:40:50 -0300

    ObtenerHorariosDisponibles_TurnosVacioUnaDisponibilidad

--- src/hooks/login/useLogin.tsx
+++ src/hooks/login/useLogin.tsx
@@ -18,8 +18,8 @@ function useLogin() {
 
   
   async function handleLogin(userName: string, password: string) {
-    let UserName = "admin";
-    let Password = "aro";
+    let UserName = "paciente";
+    let Password = "a";
     const loginData: ILogin = { UserName, Password };
 
     //consigue la info del usuario
