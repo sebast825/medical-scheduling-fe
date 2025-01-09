a9c09348a7718cb7e9ac8547cf88a6d662bdf66d
sebast825 <sebamolina825@gmail.com>
2025-01-09 20:40:57 -0300

    fixed login error

--- src/Components/General/Login/Login.tsx
+++ src/Components/General/Login/Login.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Form, Button, Row, Col } from "react-bootstrap";
 
 import useRediectHomeByRole from "../../../hooks/roles/useRediectHomeByRole";
@@ -8,6 +8,7 @@ import useLogin from "../../../hooks/login/useLogin";
 import { useQuery } from "@tanstack/react-query";
 import { Spinner } from "../../statics/Spinner";
 import { spinnerMessages } from "../../../constants/spinnerMessages";
+import { useUserInfo } from "../../../context/authContext";
 
 interface iLoginForm {
   e: () => void;
@@ -18,95 +19,83 @@ const LoginForm = ({ e }: iLoginForm) => {
   const { handleLogin, getUserInfo } = useLogin();
   const redirectByRol = useRediectHomeByRole();
   const { redirectToCreatePaciente } = useRedirects();
-  const [msgeSpinner, setMsgeSpinner]=useState<string>("");
+  const [msgeSpinner, setMsgeSpinner] = useState<string>("");
   const [loadingLogin, setLoadingLogin] = useState<boolean>(false);
+  const user = useUserInfo();
 
-  const {
-    data: handle
-  } = useQuery({
-    queryKey: ["handleLogin"],
-    queryFn: async () => {
-      await handleLogin("a", "b");      
-      return [];
-    },
-    staleTime: Infinity,
-    enabled: loadingLogin, // Habilitar la primera consulta
-  });
-
-  const {} = useQuery({
-    queryKey: ["userInfo"],
-    queryFn: async () => {
-      setMsgeSpinner(spinnerMessages.getUserInfo);
-
-      await getUserInfo();
+  useEffect(() => {
+    if (user != null) {
+      getUserInfo();
       redirectByRol();
-      setLoadingLogin(false) // saca el Spinner
-      return [];
-    },
-    staleTime: Infinity,
-    enabled: !!handle, // Habilitar solo si handleLogin se completó
-  });
- 
-  const handleSubmit = (event: any) => {
+      setLoadingLogin(false);
+    }
+  }, [user]);
+
+  const handleSubmit = async (event: any) => {
     event.preventDefault();
     setMsgeSpinner(spinnerMessages.login);
     setLoadingLogin(true);
+    handleLogin("a", "b");
   };
-
-
-
 
   return (
     <>
-    {loadingLogin ? (
-      <Spinner msge={msgeSpinner}/>
-    ) : (
-      <Row className="contenedor justify-content-center align-items-center">
-        <Col md={4} className={`paddingCol shadow-lg rounded bg-white`}>
-          <h2 className="text-center mb-2 text-primary">Iniciar Sesión</h2>
-          <Form onSubmit={handleSubmit} className="d-flex flex-column ">
-            <Form.Group controlId="formBasicNombre">
-              <Form.Label className="fw-bold"></Form.Label>
-              <Form.Control
-                type="text"
-                placeholder="Ingresar usuario"
-                onChange={(e) => setNombre(e.target.value)}
-                value={nombre}
-                className="p-2"
-              />
-            </Form.Group>
-  
-            <Form.Group controlId="formBasicPassword">
-              <Form.Label className=""></Form.Label>
-              <Form.Control
-                type="password"
-                placeholder="Ingresar contraseña"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-                className="p-2"
-              />
-            </Form.Group>
-            <div className="mt-4 d-flex flex-column">
-              <Button className="" variant="primary" type="submit">
-                Iniciar Sesión
+      {loadingLogin ? (
+        <Spinner msge={msgeSpinner} />
+      ) : (
+        <Row className="contenedor justify-content-center align-items-center">
+          <Col md={4} className={`paddingCol shadow-lg rounded bg-white`}>
+            <h2 className="text-center mb-2 text-primary">Iniciar Sesión</h2>
+            <Form onSubmit={handleSubmit} className="d-flex flex-column ">
+              <Form.Group controlId="formBasicNombre">
+                <Form.Label className="fw-bold"></Form.Label>
+                <Form.Control
+                  type="text"
+                  placeholder="Ingresar usuario"
+                  onChange={(e) => setNombre(e.target.value)}
+                  value={nombre}
+                  className="p-2"
+                />
+              </Form.Group>
+
+              <Form.Group controlId="formBasicPassword">
+                <Form.Label className=""></Form.Label>
+                <Form.Control
+                  type="password"
+                  placeholder="Ingresar contraseña"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                  className="p-2"
+                />
+              </Form.Group>
+              <div className="mt-4 d-flex flex-column">
+                <Button className="" variant="primary" type="submit">
+                  Iniciar Sesión
+                </Button>
+              </div>
+            </Form>
+            <div className="mt-2 d-flex flex-column">
+              <Button
+                onClick={redirectToCreatePaciente}
+                variant="secondary"
+                type="submit"
+              >
+                Crear Usuario
               </Button>
             </div>
-          </Form>
-          <div className="mt-2 d-flex flex-column">
-            <Button onClick={redirectToCreatePaciente} variant="secondary" type="submit">
-              Crear Usuario
-            </Button>
-          </div>
-          <div className="mt-4 d-flex flex-column align-items-end">
-            <Button variant="link" onClick={e} className="text-decoration-underline p-0">
-              Recuperar Clave
-            </Button>
-          </div>
-        </Col>
-      </Row>
-    )}
-  </>
-  
+            <div className="mt-4 d-flex flex-column align-items-end">
+              <Button
+                variant="link"
+                onClick={e}
+                className="text-decoration-underline p-0"
+              >
+                Recuperar Clave
+              </Button>
+            </div>
+          </Col>
+        </Row>
+      )}
+    </>
   );
 };
 
--- src/hooks/roles/useRediectHomeByRole.tsx
+++ src/hooks/roles/useRediectHomeByRole.tsx
@@ -15,7 +15,7 @@ import GetJwtContent, { DecodedToken } from "../../utils/jwtUtils";
       const redirectHomeByRol = async() => {
 
          if (user == null) {
-            navigate("/");
+            navigate("/login");
             return;
          };
 
