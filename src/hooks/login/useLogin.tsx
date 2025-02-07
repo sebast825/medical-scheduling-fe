2d65fc49be650c30bcc63f82c2a653e6098223fc
sebast825 <sebamolina825@gmail.com>
2025-02-07 18:25:06 -0300

    Revert "GetIndexDayOfWeekFromDate"
    This reverts commit 5d5dde58c09b5976e57d77b70d681a57116329c5.

--- src/hooks/login/useLogin.tsx
+++ src/hooks/login/useLogin.tsx
@@ -25,7 +25,6 @@ function useLogin() {
  
     try {
       const token: string = await fetchLogin(loginData);
-      console.log(token)
       cambiaLogin(token);
       return true;
     } catch (err: any) {
