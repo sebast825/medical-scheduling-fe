dff2cd5cb02732245c67e246941ec3bbb4c41853
sebast825 <sebamolina825@gmail.com>
2025-03-03 18:37:06 -0300

    createLicenseMutation handleError

--- src/hooks/License/useLicenseCacheQuery.tsx
+++ src/hooks/License/useLicenseCacheQuery.tsx
@@ -10,11 +10,14 @@ import {
   fetchGetAllLicenses,
 } from "../../services/apiService";
 import { LicenseCreateRequestDto } from "../../types/Licenses/LicenseCreateRequestDto.type";
+import useToastit from "../useToastit";
+import { ExecOptions } from "child_process";
+import { genericMessages } from "../../constants/genericMessages";
 
 function useLicenseCacheQuery() {
   const queryClient = useQueryClient();
   const user = useUserInfo();
-
+  const { error ,success} = useToastit();
   const { data: pacienteList, isLoading } = useQuery({
     queryFn: () => {
       if (user) {
@@ -26,27 +29,37 @@ function useLicenseCacheQuery() {
     queryKey: ["licenseList", user],
     staleTime: Infinity,
   });
-    const createLicenseMutation = useMutation({
-      mutationFn: (license: LicenseCreateRequestDto) =>{
-
-        if (user == null) {
-
-          console.log("hello")
-          throw new Error;
-        }
-        return fetchCreateLicenses(user, license)},
-      onSuccess: (newLicense) => {
-        queryClient.invalidateQueries({ queryKey: ["licenseList"] });
-      },
-      onError: (error) => {
-        console.log("hay un error", error);
-      },
-    });
-    function CreateLicense(license: LicenseCreateRequestDto) {
-      return createLicenseMutation.mutateAsync(license)
-    }
+  const createLicenseMutation = useMutation({
+    mutationFn: async (license: LicenseCreateRequestDto) => {
+      try {
+      if (user != null) {
+        console.log("User is not authenticated.");
+        throw new Error("User is not authenticated.");
+      }
+     
+     
+        var newLicense =  await fetchCreateLicenses("user", license);
+        success("Licensia creada correctamente")
+        return newLicense;
+      } catch (ex: any) {
+        error(genericMessages.standardError);        
+        return []
+      }
+    },
+    onSuccess: (newLicense) => {
+      queryClient.invalidateQueries({ queryKey: ["licenseList"] });
+    },
+    onError: (error) => {
+      console.error("Error creating license:", error);
+      return;
+    },
+  });
   
 
+  function CreateLicense(license: LicenseCreateRequestDto) {
+      return createLicenseMutation.mutateAsync(license);
+   
+  }
 
   return { CreateLicense };
 }
