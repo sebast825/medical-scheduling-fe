2125140d29e02e2eea6ebb0db8412e16f02083c2
sebast825 <sebamolina825@gmail.com>
2025-03-07 17:51:09 -0300

    redirect home from license

--- src/pages/administrador/LicensesMedico.tsx
+++ src/pages/administrador/LicensesMedico.tsx
@@ -1,10 +1,20 @@
+import { useEffect } from "react";
 import BackLink from "../../Components/buttons/BackLink/BackLink";
 import Opening from "../../Components/General/Opening/Opening";
 import TableLicense from "../../Components/License/TableLicense/TableLicense";
+import useIsAdministrador from "../../hooks/roles/useIsAdministrador";
+import { useRedirectToLogin } from "../../routes/navigation";
 import { LicenseResponseDto } from "../../types/Licenses/LicenseResponseDto.type";
 
 function LicensesMedico() {
+  const isAdministrador = useIsAdministrador();
+  const redirectToLogin = useRedirectToLogin();
+  useEffect(() => {
+  
+    if (!isAdministrador) redirectToLogin();
 
+    
+  }, []);
   return (
     <>
       <div className="mb-2 mb-md-5">
